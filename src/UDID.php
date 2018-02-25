29bf7f343981317194a152627b90b088f274b57b
MrARM <jordan@thesmokinggnu.us>
2018-02-25 16:50:54 -0600

    Create Basic functions without any fancy stuff yet
    Signed-off-by: MrARM <jordan@thesmokinggnu.us>

--- src/UDID.php
+++ src/UDID.php
@@ -11,5 +11,21 @@ namespace mrarm;
 
 class UDID
 {
+    public static function CalculateUDID($serial,$ecid,$wifimac,$btmac){
+        //Run regex to see if info is valid
+        //Regex list
+        $mac_address_regex = "/((\d|([A-F])){2}:){5}(\d|([A-F])){2}/";
+        //ECID and serial are coming later.
 
+        //For the mac address
+        foreach(Array($wifimac,$btmac) as $mac){
+            if(!preg_match($mac_address_regex,$mac)){
+                return "Error"; //Exception coming soon
+            }
+        }
+
+        //Do the calculation
+        $UDID = sha1($serial.$ecid.$wifimac.$btmac);
+        return $UDID;
+    }
 }
