e7e8716858749c8b7a7949f3302b0d3f63930fbb
MrARM <jordan@thesmokinggnu.us>
2018-02-25 16:58:58 -0600

    Add phpdoc and ecid check
    Signed-off-by: MrARM <jordan@thesmokinggnu.us>

--- src/UDID.php
+++ src/UDID.php
@@ -6,26 +6,39 @@
  * Time: 3:31 PM
  */
 
-namespace mrarm;
+namespace mrarm {
 
 
-class UDID
-{
-    public static function CalculateUDID($serial,$ecid,$wifimac,$btmac){
-        //Run regex to see if info is valid
-        //Regex list
-        $mac_address_regex = "/((\d|([A-F])){2}:){5}(\d|([A-F])){2}/";
-        //ECID and serial are coming later.
+    class UDID
+    {
+        /**
+         * Do a UDID calculation for iPhone 4+
+         * @param string int $ecid numeric ECID
+         * @param string $wifimac Wifi mac address
+         * @param string $btmac Bluetooth Mac address
+         * @return string UDID
+         */
+        public static function CalculateUDID($serial, $ecid, $wifimac, $btmac){
+            //Run regex to see if info is valid
+            //Regex list
+            $mac_address_regex = "/((\d|([A-F])){2}:){5}(\d|([A-F])){2}/";
+            //ECID and serial are coming later.
 
-        //For the mac address
-        foreach(Array($wifimac,$btmac) as $mac){
-            if(!preg_match($mac_address_regex,$mac)){
+            //For the mac address
+            foreach(Array($wifimac,$btmac) as $mac){
+                if(!preg_match($mac_address_regex,$mac)){
+                    return "Error"; //Exception coming soon
+                }
+            }
+
+            //ECID
+            if(!is_numeric($ecid)){
                 return "Error"; //Exception coming soon
             }
-        }
 
-        //Do the calculation
-        $UDID = sha1($serial.$ecid.$wifimac.$btmac);
-        return $UDID;
+            //Do the calculation
+            $UDID = sha1(strtoupper($serial).$ecid.$wifimac.$btmac);
+            return $UDID;
+        }
     }
 }
